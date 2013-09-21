6cb5f4830a3aa6e4e9a1294f8bf3a4abda7e684f
mlinsenbardt <mlinsenbardt@mail.csuchico.edu>
2013-09-21 15:04:39 -0700

    Filenames working

--- p3/rfind.rb
+++ p3/rfind.rb
@@ -3,8 +3,21 @@
 filename = ARGV[0]
 
 puts "Files with names that matches <#{filename}>"
+sname = `find . -name  '\*.rb' -o -name '\*.html' -o -name '\*.erb' -o -name '\*.js' -o -name '\*.css' -o -name '\*.yml' -o -name '\*.txt'`
+sarr = sname.split("\n")
+sarr.select! { |str|    str.include? "#{filename}"}
+	 #	|| str.include? ".html" 
+	#	|| str.include? ".erb"
+	#	|| str.include? ".js" 
+	#	|| str.include? ".css" 
+	#	|| str.include? ".yml"
+	#	|| str.include? ".txt" 
+sarr.each { |str| print "  ", str, "\n"}
+
+
 puts '*' *50
-puts `find . -name  "#{filename}" && /\.rb$ -o -name /\.html$`
 puts "Files with content that matches <#{filename}>"
-puts `grep -r -i -n "#{filename}" .` 
+a = string=`grep -r -i -n "#{filename}" .`
+arr = a.split('\n')
+puts arr
 
