acd20a0064fd7c6239469cd361dbf53bd63e12dc
mlinsenbardt <mlinsenbardt@mail.csuchico.edu>
2013-09-22 15:01:15 -0700

    Basically done

--- p3/rfind.rb
+++ p3/rfind.rb
@@ -13,11 +13,18 @@ puts "Files with content that matches <#{filename}>"
 a = string=`grep -r -i -n --include=*.rb --include=*.html --include=*.erb --include=*.js --include=*.css --include=*.yml --include=*.txt "#{filename}" .`
 arr = a.split("\n")
 arr.sort_by! { |str| str }
-arr.each { |str|  str.gsub!(/\:/,"\n")
+arr.map! { |str|  str.gsub!(/\:/,"\n")
 		arr2 = str.split("\n")
-		puts arr2[0]
-		puts "  #{arr2[1]}:#{arr2[2]}"
-		if str != arr[-1]
-			puts '-' *50 
+		arr2[1] = "  #{arr2[1]}:#{arr2[2]}"
+		arr2.pop
+		str = arr2
+	}
+arr.flatten!
+arr.uniq!
+arr.each { |str| 
+		if str[0] == "."
+			puts '-' *50
+			puts "#{str}\n"
+		else
+			puts "#{str}\n"
 		end}
-
