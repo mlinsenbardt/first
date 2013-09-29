287d7e53b0c57947058ce203758ae7262f90f7c2
mlinsenbardt <mlinsenbardt@mail.csuchico.edu>
2013-09-29 15:56:50 -0700

    Think input is working

--- p4/hangman.rb
+++ p4/hangman.rb
@@ -8,28 +8,30 @@ f.each do  |line|
 puts "Enter word length between 5-20: "
 range = gets 
  
-words.map! { |string| string.strip!
-		if string.size == range.to_i
-			string
-		else
-			string.clear
-		end}.uniq!
-ATTEMPTS = 10
-guesses = Array.new(range)
+words.select! { |string| string.strip!
+		string.size == range.to_i}
+puts words
+attempts = 10
+guesses = Array.new(range.to_i)
 out = "_____"
-while ATTEMPTS > 0
+while attempts > 0
 	puts "enter your guess: "
 	guesses.push(gets)
-	if words.select { |string| string.include?(guesses[-1])}.count != words
+	if words.select { |string| string.include?(guesses[-1])} != words
 		words.map! { |string| if string.include?(guesses[-1])
 					string.clear
 				else
 					string
 				end}.uniq!
-		ATTEMPTS -= 1
+		attempts -= 1
+		guesses.each { |string| if string
+						string.strip!
+						print "#{string} "
+					end}
+		puts "(#{attempts} chances left)\n"
 	else
 		target = words[0]
-		guesses.each	
-	puts ATTEMPTS
+		guesses.each
+	end	
 end
-#puts words
+puts words
