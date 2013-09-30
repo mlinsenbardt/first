5a3928537e1cc79e2920dba5c19bfc5178d5422d
mlinsenbardt <mlinsenbardt@mail.csuchico.edu>
2013-09-29 20:31:06 -0700

    Implemented type checking and checking for duplicates

--- p4/hangman.rb
+++ p4/hangman.rb
@@ -7,7 +7,11 @@ f.each do  |line|
 	 end
 puts "Enter word length between 5-20: "
 range = gets 
- 
+if range.to_i <5
+	range = "5"
+elsif range.to_i >20
+	range = "20"
+end
 words.select! { |string| string.strip!
 		string.size == range.to_i}
 attempts = 10
@@ -16,24 +20,32 @@ out = '_' *range.to_i
 target = ""
 while attempts > 0 && out != target
 	puts "enter your guess: "
-	guesses.push(gets)
-	if words.select { |string| string.include? guesses[-1][0]}.count != words.count && target == ""
-		words.delete_if { |string| string.include? guesses[-1][0]}	
-		attempts -= 1
-	elsif target == ""
-		target = words[0]
-		out[target.index(guesses[-1][0])] = guesses[-1][0]
-	elsif target.index(guesses[-1][0])
-		out[target.index(guesses[-1][0])] = guesses[-1][0]
+	guess = gets[0]
+	if guesses.include? guess
+		puts "you already guessed #{guess}\n"
+	elsif !guess.index(/[a-z]/)
+		puts "only lowercase letters a-z are allowed\n"
 	else
-		attempts -= 1
-	end
+		guesses.push(guess)
+		#if guesses[-1][0].is_a?
+		if words.select { |string| string.include? guesses[-1][0]}.count != words.count && target == ""
+			words.delete_if { |string| string.include? guesses[-1][0]}	
+			attempts -= 1
+		elsif target == ""
+			target = words[0]
+			out[target.index(guesses[-1][0])] = guesses[-1][0]
+		elsif target.index(guesses[-1][0])
+			out[target.index(guesses[-1][0])] = guesses[-1][0]
+		else
+			attempts -= 1
+		end
 		guesses.each { |string| if string
 						string.strip!
 						print "#{string} "
 					end}
 		puts "(#{attempts} chances left)\n"	
 		puts out
+	end
 end
 	if out == target
 		puts "\nYOU WIN!\n"
