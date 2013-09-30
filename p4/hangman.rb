af388262a27fb3519509a5ee8d88f583883edbe7
mlinsenbardt <mlinsenbardt@mail.csuchico.edu>
2013-09-29 19:05:26 -0700

    Implemented shrinking dictionary

--- p4/hangman.rb
+++ p4/hangman.rb
@@ -13,25 +13,27 @@ words.select! { |string| string.strip!
 puts words
 attempts = 10
 guesses = Array.new(range.to_i)
-out = "_____"
+out = '_' *range.to_i
 while attempts > 0
 	puts "enter your guess: "
 	guesses.push(gets)
-	if words.select { |string| string.include?(guesses[-1])} != words
-		words.map! { |string| if string.include?(guesses[-1])
-					string.clear
-				else
-					string
-				end}.uniq!
+	if words.select { |string| string.include? guesses[-1][0]}.count != words.count
+		words.delete_if { |string| string.include? guesses[-1][0]}	
 		attempts -= 1
+		puts words 
+		puts guesses[-1]
+	else
+		target = words[0]
+		out[target.index(guesses[-1][0])] = guesses[-1][0]
+	end
 		guesses.each { |string| if string
 						string.strip!
 						print "#{string} "
 					end}
-		puts "(#{attempts} chances left)\n"
-	else
-		target = words[0]
-		guesses.each
-	end	
+		puts "(#{attempts} chances left)\n"	
+		puts out
 end
 puts words
+
+
+
